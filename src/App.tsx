22709307411d74244e19b3e7616e26420bffad0b
Kvazar-213452 <kvazar382@gmail.com>
2025-01-19 00:29:41 +0200

    end

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './assets/style/App.css'
+import ProgramList from "./ProgramList";
+import { data } from "./data";
 
 function App() {
   return (
@@ -8,13 +10,11 @@ function App() {
         <h1 className='main_name'>Kvazar-213452</h1>
         <h3>Full-stack Developer</h3>
         <div className='my_url'>
-          <a target="_blank" href="https://github.com/Kvazar-213452">https://github.com/Kvazar-213452</a>
-          <a href="https://github.com/Kvazar-213452">https://github.com/Kvazar-213452</a>
-          <a href="https://github.com/Kvazar-213452">https://github.com/Kvazar-213452</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Kvazar-213452">https://github.com/Kvazar-213452</a>
         </div>
       </div>
       <div className='main_1'>
-        <h1>d</h1>
+        <ProgramList data={data} />
       </div>
     </div>
     </>
---
+++ src/data.ts
@@ -0,0 +1,29 @@
+export const data = [
+    {
+        url: "http://localhost:5173/",
+        name: "Security hub",
+        time: "3 month",
+        img: "src/assets/img/image_0.png",
+        lang: [
+            "C/C++", 
+            "SCSS", 
+            "JS",
+            "HTML",
+            "Golang",
+            "Rust",
+            "Node.js",
+            "Python",
+            "C#",
+            "PHP"
+        ],
+        desc: "During the research, the Security Hub program was developed to protect and optimize the PC. It is implemented in C/C++, Golang, Python, JavaScript, HTML, CSS, Rust. Security Hub combines antivirus, system cleaning, network management and data encryption.<br /><br />The program has a modular architecture, integration with the VirusTotal API and monitoring functions. It is a multifunctional tool for analyzing resources, detecting threats and improving PC performance.",
+    },
+    {
+        url: "http://localhost:5173/",
+        name: "Security hub",
+        time: "3 mosjf",
+        img: "src/assets/img/image_0.png",
+        lang: ["C/C++", "SCSS", "JS"],
+        desc: "During the research, the Security Hub program was developed to protect and optimize the PC. It is implemented in C/C++, Golang, Python, JavaScript, HTML, CSS, Rust. Security Hub combines antivirus, system cleaning, network management and data encryption.<br /><br />The program has a modular architecture, integration with the VirusTotal API and monitoring functions. It is a multifunctional tool for analyzing resources, detecting threats and improving PC performance.",
+    },
+];
